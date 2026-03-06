bfb9ac39ed766b70a41f27c640b6c6a00b192978
liaocifer <guan-lun@agmigroup.com>
2026-03-06 01:21:54 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,1 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyBLe1XpD5sB3zyKatf7uliAyD4ph9cnc8o",
-  authDomain: "banqi-fb0af.firebaseapp.com",
-  projectId: "banqi-fb0af",
-  storageBucket: "banqi-fb0af.firebasestorage.app",
-  messagingSenderId: "1037871147893",
-  appId: "1:1037871147893:web:3d2cae1c440dc43331813c"
-};
+
