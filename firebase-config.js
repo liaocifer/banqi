4ff20fe2ab8d94ae8e5ce4c24bf3a092cdef31b7
liaocifer <guan-lun@agmigroup.com>
2026-03-05 22:13:16 -0800

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
-// 複製此檔為 firebase-config.js 並填入你的 Firebase 專案設定
-// 1. 前往 https://console.firebase.google.com/ 建立專案
-// 2. 啟用 Firestore Database（建立資料庫）
-// 3. 專案設定 > 一般 > 你的應用程式 > 新增 Web 應用程式，取得設定
-// 4. 將下面的 placeholder 換成你的 apiKey、authDomain 等
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-window.firebaseConfig = {
+// Your web app's Firebase configuration
+const firebaseConfig = {
   apiKey: "AIzaSyBLe1XpD5sB3zyKatf7uliAyD4ph9cnc8o",
   authDomain: "banqi-fb0af.firebaseapp.com",
   projectId: "banqi-fb0af",
@@ -12,3 +12,6 @@ window.firebaseConfig = {
   messagingSenderId: "1037871147893",
   appId: "1:1037871147893:web:3d2cae1c440dc43331813c"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
