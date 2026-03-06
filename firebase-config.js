72c103ab8015d48ec1b7c1088f4723e8ce79d423
liaocifer <guan-lun@agmigroup.com>
2026-03-05 21:54:26 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,3 @@
-// 複製此檔為 firebase-config.js 並填入你的 Firebase 專案設定
-// 1. 前往 https://console.firebase.google.com/ 建立專案
-// 2. 啟用 Firestore Database（建立資料庫）
-// 3. 專案設定 > 一般 > 你的應用程式 > 新增 Web 應用程式，取得設定
-// 4. 將下面的 placeholder 換成你的 apiKey、authDomain 等
-
 window.firebaseConfig = {
   apiKey: "AIzaSyBLe1XpD5sB3zyKatf7uliAyD4ph9cnc8o",
   authDomain: "banqi-fb0af.firebaseapp.com",
