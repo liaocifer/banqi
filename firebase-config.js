fc5f9d82612db3d482b2e3fd2299fe26c74006ea
liaocifer <guan-lun@agmigroup.com>
2026-03-05 22:19:55 -0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,4 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+window.firebaseConfig = {
   apiKey: "AIzaSyBLe1XpD5sB3zyKatf7uliAyD4ph9cnc8o",
   authDomain: "banqi-fb0af.firebaseapp.com",
   projectId: "banqi-fb0af",
@@ -12,6 +6,3 @@ const firebaseConfig = {
   messagingSenderId: "1037871147893",
   appId: "1:1037871147893:web:3d2cae1c440dc43331813c"
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
