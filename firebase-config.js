724bfb8209401e674a06772e45de305730a11410
liaocifer <guan-lun@agmigroup.com>
2026-03-06 23:57:05 -0800

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.firebaseConfig = {
-  apiKey: "AIzaSyApJChaPDPfQBbDVHHqxg3wmaI-xMiE38I",
-  authDomain: "banqi-game-e3f33.firebaseapp.com",
-  databaseURL: "https://banqi-game-e3f33-default-rtdb.firebaseio.com",
-  projectId: "banqi-game-e3f33",
-  storageBucket: "banqi-game-e3f33.firebasestorage.app",
-  messagingSenderId: "948286345507",
-  appId: "1:948286345507:web:b20c2b6876a39fee74bc96"
-};
